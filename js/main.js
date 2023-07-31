d061bd9a3a8453e4d3acc0f57e7b1304428bd6d9
Prueba1-Jorge <jorgeexamen1.1@gmail.com>
2023-07-31 11:00:18 -0500

    Cuarto Commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// Declaración de una variable
+let nombre = "Juan";
+
+// Impresión en la consola
+console.log("Hola " + nombre + "!");
+
+// Función que devuelve el cuadrado de un número
+function calcularCuadrado(numero) {
+  return numero * numero;
+}
+
+// Llamada a la función y almacenamiento del resultado en una variable
+let resultado = calcularCuadrado(5);
+
+// Impresión del resultado en la consola
+console.log("El cuadrado de 5 es: " + resultado);
